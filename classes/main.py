4af72c9d33b93b9aad85897642727f21b9ef99b2
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-05-25 21:14:25 -0700

    Update files
    Add random damage counter

--- classes/main.py
+++ classes/main.py
@@ -2,6 +2,10 @@ from classes.game import Person, bcolors
 
 magic = [{"name": "Fire", "cost": 10, "dmg": 60},
          {"name": "Thunder", "cost": 10, "dmg": 60},
-         {"name": "Blizzard", "cost": 10, "dmg": 60}]
+         {"name": "fire", "cost": 10, "dmg": 60}]
 
 player = Person(460, 65, 60, 34, magic)
+
+print(player.generate_damage(0))
+print(player.generate_damage(0))
+print(player.generate_damage(0))
