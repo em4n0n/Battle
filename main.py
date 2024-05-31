387edf77466f3e13e40ec33191ddec2ea94584b1
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-05-30 21:58:38 -0700

    Update game/main
    add bcolors example

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from classes.game import Person, bcolors
 
 magic = [{"name": "Fire", "cost": 10, "dmg": 60},
-         {"name": "Thunder", "cost": 10, "dmg": 80},
+         {"name": "Thunder", "cost": 12, "dmg": 80},
          {"name": "Blizzard", "cost": 10, "dmg": 60}]
 
-player = Person(460, 65, 60, 34, magic)
-print(player.generate_spell_damage(0))
-print(player.generate_spell_damage(1))
+player = Person(460, 65, 60, 34, magic) # instantiate player
+enemy = Person(1200, 65, 45, 25, magic) # instantiate (creating object from a blueprint(enemy 
+
+running = True
+i = 0
+
+print(bcolors.FAIL + bcolors.BOLD + "AN ENEMY ATTAKCS!" + bcolors.ENDC)
