409f668ac36944490f36f518bd4f5385284b762f
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-06-01 20:18:33 -0700

    Update main.py
    add more damage to magic spells

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from classes.game import Person, bcolors
 
-magic = [{"name": "Fire", "cost": 10, "dmg": 60},
-         {"name": "Thunder", "cost": 12, "dmg": 80},
-         {"name": "Blizzard", "cost": 10, "dmg": 60}]
+magic = [{"name": "Fire", "cost": 10, "dmg": 100},
+         {"name": "Thunder", "cost": 12, "dmg": 124},
+         {"name": "Blizzard", "cost": 10, "dmg": 100}]
 
 player = Person(460, 65, 60, 34, magic) # instantiate player
 enemy = Person(1200, 65, 45, 25, magic) # instantiate (creating object from a blueprint(enemy 
