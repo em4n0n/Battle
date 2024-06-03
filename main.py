8c54195bd0ec2fd8fe19b68f20d516186f1cc9eb
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-06-02 21:56:27 -0700

    Update main.py
    remove magic to add player_magic

--- main.py
+++ main.py
@@ -12,10 +12,6 @@ quake = Spell("Quake", 14, 140, "black")
 cure = Spell("Cure", 12, 120, "white")
 cura = Spell("Cura", 18, 200)
 
-magic = [{"name": "Fire", "cost": 10, "dmg": 100},
-         {"name": "Thunder", "cost": 12, "dmg": 124},
-         {"name": "Blizzard", "cost": 10, "dmg": 100}]
-
 player = Person(460, 65, 60, 34, magic) # instantiate player
 enemy = Person(1200, 65, 45, 25, magic) # instantiate (creating object from a blueprint(enemy 
 
