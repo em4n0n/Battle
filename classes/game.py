6394e912bc3cf280ad279798dca9610fdce2da2a
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-06-15 19:35:09 -0700

    Update hp_bar increment

--- classes/game.py
+++ classes/game.py
@@ -79,14 +79,17 @@ class Person:
             i += 1
     
     def get_stats(self):
-        bar = ""
+        hp_bar = ""
         bar_ticks = (self.hp / self.maxhp) * 100 / 4
 
         while bar_ticks > 0:
-            bar += " "
+            hp_bar += " "
+            bar_ticks -= 1
+
+        while len(hp_bar) < 25:
+            hp_bar += " "
 
         print("                     ________________________             __________ ")
         print(bcolors.BOLD + self.name + "    " +
-            str(self.hp) + "/" + str(self.maxhp) + " |" +  bcolors.OKGREEN + "████████               " + bcolors.ENDC + bcolors.BOLD
-            + "|     "+
+            str(self.hp) + "/" + str(self.maxhp) + " |" + bcolors.OKGREEN +  hp_bar + bcolors.ENDC + "|     " +
             str(self.mp) + "/" + str(self.maxmp) + " |" + bcolors.OKBLUE + "█████████|" + bcolors.ENDC + "|")
