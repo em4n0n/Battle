95e16d1fa210e5467af9b35ebc515d89c4279782
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-06-27 21:57:59 -0700

    Fix HP/MP string

--- classes/game.py
+++ classes/game.py
@@ -137,11 +137,10 @@ class Person:
 
         while len(mp_bar) < 10:
             mp_bar += " "
-            mp_ticks -+ 1
 
         hp_string = str(self.hp) + "/" + str(self.maxhp)
         current_hp = ""
-        
+
         if len(hp_string) < 9:
             decreased = 9 - len(hp_string)
 
@@ -149,9 +148,23 @@ class Person:
                 current_hp += " "
                 decreased -= 1
 
-            current_hp += hp_string
+                current_hp += hp_string
         else:
             current_hp = hp_string
+
+
+        mp_string = str(self.hp) + "/" + str(self.maxmp)
+        current_hp = ""
+        
+        if len(mp_string) < 7:
+            decreased = 7 - len(hp_string)
+            while decreased > 0:
+                current_mp += " "
+                decreased -= 1
+
+            current_mp += mp_string
+        else:
+            current_mp = hp_string
 
         mp_string = str(self.mp) + "/" + str(self.maxmp)
         current_mp = ""
